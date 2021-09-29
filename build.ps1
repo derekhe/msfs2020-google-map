6900eb01be10fcf9238a9e73b7d0848662665940
derekhe <derekhe@april1985.com>
2021-09-29 20:51:41 +0800

    Add build scripts; Modify run.bat to select user installed python or embedded python

---
+++ build.ps1
@@ -0,0 +1,2 @@
+rm -r .\dist\
+7z -xr@7zignore a .\dist\msfs2020-google-map-$((Get-Date).ToString("yyyy-MM-dd")).zip
--- run.bat
+++ run.bat
@@ -1,6 +1,13 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-py -m pip install -r requirements.txt
-py src\app.py
+if exist .\Python39\python.exe (
+    echo "Use embedded python"
+    .\Python39\python.exe src\app.py
+) else (
+    echo "Use user python"
+    py -m pip install -r requirements.txt
+    py src\app.py
+)
+
 pause
