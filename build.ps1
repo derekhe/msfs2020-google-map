686e8ff2a097c13b31729770b548783242ceaeea
derekhe <derekhe@april1985.com>
2021-11-21 17:41:27 +0800

    Use prod config when release

--- build.ps1
+++ build.ps1
@@ -3,4 +3,6 @@ cd Python39
 .\Python.exe -m pip install -r ..\requirements.txt
 cd ..
 
+copy config-prod.ini config.ini
+
 7z -xr@7zignore a .\dist\msfs2020-google-map-$((Get-Date).ToString("yyyy-MM-dd")).zip
